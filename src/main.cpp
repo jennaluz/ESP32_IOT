7ad170dbf65f3121e62ed9665869bbd6661ca372
jennaluz <jpura827@gmail.com>
2023-03-28 20:21:22 -0700

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <Arduino.h>
 
 
-// Replace with your network credentals
-const char *ssid = "jenna-luz's phone";
-const char *password = "iphone14promax";
+// network credentials
+const char *ssid = "redacted";
+const char *password = "redacted";
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -12,9 +12,9 @@ WiFiServer server(80);
 // Variable to store the HTTP request
 String header;
 
-unsigned long current_time = millis(); // Current time
-unsigned long previous_time = 0;       // Previous time
-const long timeout_time = 2000;        // Define timeout time in milliseconds (example: 2000ms = 2s)
+unsigned long current_time = millis(); // current time
+unsigned long previous_time = 0;       // previous time
+const long timeout_time = 2000;        // define timeout time in milliseconds (example: 2000ms = 2s)
 
 
 void ui_handler(void *parameters);
@@ -49,32 +49,30 @@ void loop() {}
 
 void ui_handler(void *parameters) {
     while (true) {
-        WiFiClient client = server.available();   // Listen for incoming clients
+        WiFiClient client = server.available();   // listen for incoming clients
 
-        if (client) {                             // If a new client connects,
+        // new client connects
+        if (client) {
             current_time = millis();
             previous_time = current_time;
-            Serial.println("New Client.");          // print a message out in the serial port
-            String current_line = "";                // make a String to hold incoming data from the client
+            Serial.println("New Client.");
+            String current_line = "";
 
-            while (client.connected() && current_time - previous_time <= timeout_time) {  // loop while the client's connected
+            // loop while the client is connected
+            while (client.connected() && current_time - previous_time <= timeout_time) {
                 current_time = millis();
-                if (client.available()) {             // if there's bytes to read from the client,
-                    char c = client.read();             // read a byte, then
-                    Serial.write(c);                    // print it out the serial monitor
+
+                if (client.available()) {
+                    char c = client.read();
+                    Serial.write(c);
                     header += c;
-                    if (c == '\n') {                    // if the byte is a newline character
-                                                        // if the current line is blank, you got two newline characters in a row.
-                                                        // that's the end of the client HTTP request, so send a response:
+                    if (c == '\n') {
                         if (current_line.length() == 0) {
-                            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                            // and a content-type so the client knows what's coming, then a blank line:
                             client.println("HTTP/1.1 200 OK");
                             client.println("Content-type:text/html");
                             client.println("Connection: close");
                             client.println();
 
-                            // Display the HTML web page
                             client.println(
                                 "<!DOCTYPE html>"
                                 "<html>"
@@ -106,9 +104,9 @@ void ui_handler(void *parameters) {
                 }
             }
 
-            // Clear the header variable
+            // clear the header variable
             header = "";
-            // Close the connection
+            // close the connection
             client.stop();
             Serial.println("Client disconnected.");
             Serial.println("");
